dec3ecaae88938804bfd3d7646a1952c42c1e255
Avidan Baruchi <avidanb@codevalue.net>
2016-09-13 00:27:08 +0300

    start page is ready to start build the app

---
+++ PriceCompare/WebApi/App/app.js
@@ -0,0 +1,19 @@
+﻿(function (angualar) {
+    var app = angular.module("PricesApp",
+    [
+        "ngAnimate",
+        "ui.router",
+        "ui.bootstrap"
+    ]);
+
+    app.config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('main',
+        {
+            template: '<div>I am Template</div>',
+            controller: "MainController as vm",
+            url: '/main'
+        });
+
+        $urlRouterProvider.otherwise('/main');
+    });
+})(angular);
---
+++ PriceCompare/WebApi/App/controllers/mainCtrl.js
@@ -0,0 +1,9 @@
+﻿(function (angular) {
+    angular.module("PricesApp")
+        .controller("MainController", MainController);
+
+    function MainController() {
+        this.data = "tada";
+    }
+
+})(angular);
